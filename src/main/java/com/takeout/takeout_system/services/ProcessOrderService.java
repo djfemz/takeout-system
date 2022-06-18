18d62c56b3e6379e5ece0966222bef599fdb4eea
djfemz <oladejifemi00@gmail.com>
2022-06-18 19:34:30 +0100

    added services

--- src/main/java/com/takeout/takeout_system/services/ProcessOrderService.java
+++ src/main/java/com/takeout/takeout_system/services/ProcessOrderService.java
@@ -2,8 +2,11 @@ package com.takeout.takeout_system.services;
 
 import com.takeout.takeout_system.data.dto.EnterItemRequest;
 
+import java.math.BigInteger;
+
 public interface ProcessOrderService {
     Boolean makeNewOrder();
     Boolean enterItem(EnterItemRequest enterItemRequest);
-
+    BigInteger endOrder();
+    Boolean makeCashPayment(BigInteger amount);
 }
