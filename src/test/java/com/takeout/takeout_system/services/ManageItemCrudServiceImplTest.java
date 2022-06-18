a395852b567ff5725a95126e42ff744bdc1780b3
djfemz <oladejifemi00@gmail.com>
2022-06-18 20:18:20 +0100

    generated tests

---
+++ src/test/java/com/takeout/takeout_system/services/ManageItemCrudServiceImplTest.java
@@ -0,0 +1,26 @@
+package com.takeout.takeout_system.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManageItemCrudServiceImplTest {
+
+    @Test
+    void createItemTest() {
+
+    }
+
+
+    @Test
+    void findItemTest() {
+    }
+
+    @Test
+    void modifyItemTest() {
+    }
+
+    @Test
+    void deleteItemTest() {
+    }
+}
