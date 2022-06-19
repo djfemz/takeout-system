54475efc80c1a2cd6942e1fe50f23340f959ed21
djfemz <oladejifemi00@gmail.com>
2022-06-19 04:01:38 +0100

    added delivery service tests

--- src/main/java/com/takeout/takeout_system/data/models/Delivery.java
+++ src/main/java/com/takeout/takeout_system/data/models/Delivery.java
@@ -1,13 +1,22 @@
 package com.takeout.takeout_system.data.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Delivery {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private String id;
+    private Long id;
     private String name;
     private String attribute3;
     @OneToMany
--- src/main/java/com/takeout/takeout_system/services/ManageDeliveryServiceImpl.java
+++ src/main/java/com/takeout/takeout_system/services/ManageDeliveryServiceImpl.java
@@ -1,12 +1,24 @@
 package com.takeout.takeout_system.services;
 
 import com.takeout.takeout_system.data.dto.CreateDeliveryRequest;
+import com.takeout.takeout_system.data.models.Delivery;
+import com.takeout.takeout_system.data.repositories.DeliveryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class ManageDeliveryServiceImpl implements ManageDeliveryService{
+    @Autowired
+    private DeliveryRepository deliveryRepository;
+
     @Override
     public Boolean createDelivery(CreateDeliveryRequest createDeliveryRequest) {
-        return null;
+        Delivery delivery = new Delivery();
+        delivery.setName(createDeliveryRequest.getName());
+        delivery.setSale(new ArrayList<>());
+        deliveryRepository.save(delivery);
+        return true;
     }
 }
